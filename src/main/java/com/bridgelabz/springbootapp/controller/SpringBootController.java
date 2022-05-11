526bba8ee361503826cc6a60748b85473c7e6e5b
Shubham <ssp671995@gmail.com>
2022-05-11 20:29:46 +0530

    Added the rest call to show message pass using postmapping for first name and last name in the spring boot program.

--- src/main/java/com/bridgelabz/springbootapp/controller/SpringBootController.java
+++ src/main/java/com/bridgelabz/springbootapp/controller/SpringBootController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.springbootapp.controller;
 
+import com.bridgelabz.springbootapp.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,18 +12,21 @@ public class SpringBootController {
 
     @RequestMapping(value = {"/hello-w"}, method = RequestMethod.GET)
     public String sayHelloDifferently() {
-
         return "Hello Everyone";
     }
 
     @GetMapping("/hello/query")
     public String sayPosting(@RequestParam String firstName, @RequestParam String lastName) {
-
         return "Hello " + firstName + " " + lastName;
     }
 
     @GetMapping("/hello/{firstName}")
     public String sayHello(@PathVariable String firstName) {
         return "Hello " + firstName;
+    }
+
+    @PostMapping("/hello/post")
+    public String sayPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName();
     }
 }
